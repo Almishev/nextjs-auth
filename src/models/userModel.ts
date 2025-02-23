04544b72ccb2b1e87950a91b35c07f885cd8f2ea
Anton Almishev <antonalmishev@abv.bg>
2025-02-23 14:41:04 +0200

    test users

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,13 +1,32 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.models.User?.schema || new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
     isAdmin: {
         type: Boolean,
-        default: false
-    }
+        default: false,
+    },
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
 }, {
     timestamps: true
 });
